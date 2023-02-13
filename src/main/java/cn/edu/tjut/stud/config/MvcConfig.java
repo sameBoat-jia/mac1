adbc0e659c88f8d055465bda0785b0c046c8bb82
贾同舟 <jtz123123@qq.com>
2023-02-13 17:38:47 +0800

    test

--- src/main/java/cn/edu/tjut/stud/config/MvcConfig.java
+++ src/main/java/cn/edu/tjut/stud/config/MvcConfig.java
@@ -1,8 +1,13 @@
 package cn.edu.tjut.stud.config;
 
+import cn.edu.tjut.stud.common.JacksonObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurationSupport
@@ -12,11 +17,11 @@ public class MvcConfig extends WebMvcConfigurationSupport
         registry.addResourceHandler("/**").addResourceLocations("classpath:/");
     }
 
-//    @Override
-//    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        MappingJackson2HttpMessageConverter messageConverter=new MappingJackson2HttpMessageConverter();
-//        messageConverter.setObjectMapper(new JacksonObjectMapper());
-//        converters.add(0,messageConverter);
-//
-//    }
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter messageConverter=new MappingJackson2HttpMessageConverter();
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        converters.add(0,messageConverter);
+
+    }
 }
